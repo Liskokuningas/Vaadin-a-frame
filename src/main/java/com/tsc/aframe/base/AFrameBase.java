0fc1c64b6a16841c991e7c62df6f535b2d49027d
Juho Piirainen <Juho Piirainen>
2019-12-29 21:37:49 +0200

    correctled misused attributes list

--- src/main/java/com/tsc/aframe/base/AFrameBase.java
+++ src/main/java/com/tsc/aframe/base/AFrameBase.java
@@ -1,12 +1,15 @@
 package com.tsc.aframe.base;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import com.tsc.aframe.interfaces.IAframeBase;
 import com.tsc.aframe.utils.Gulper;
 import com.vaadin.flow.component.Component;
 
 public class AFrameBase extends Component implements IAframeBase {
+	
+	protected Map<String, String> attributesMap = new HashMap<String, String>();
 	
 	public AFrameBase() {
 				
@@ -20,5 +23,26 @@ public class AFrameBase extends Component implements IAframeBase {
 			});
 		}
 	
-	}	
+	}
+
+	
+	public Map<String, String> getAttributesMap() {
+		return attributesMap;		
+	}
+	
+	public void setAttribute(String attribute, String value) {
+		this.getElement().setAttribute(attribute, value);
+	}
+	
+	public String getAttribute(String attribute) {
+		return this.getElement().getAttribute(attribute);
+	}
+	
+	public AFrameBase addXml(String elementXml) {
+		AFrameBase aFrameBaseObj = Gulper.aFrameFactory(elementXml);
+		if ( aFrameBaseObj != null ) {
+			this.add(aFrameBaseObj);
+		}
+		return aFrameBaseObj;
+	}
 }
